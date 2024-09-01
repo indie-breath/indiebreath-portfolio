5daa55d41851172c61bcea7bd7f4d6502e0f6334
indiebreath <indiebreath@proton.me>
2024-09-01 13:20:43 +1000

    HTML mostly done, CSS halfway done

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
+const router = express.Router();
 
-const port = 3001
+const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.use("/", express.static(__dirname + "/views"));
+app.use(express.static(__dirname + "/public"));
+
+router.get("/", function (req, res) {
+    res.sendFile(__dirname + "/views/index.html");
 });
 
 app.listen(port, () => {
-    console.log("App running on port 3001.");
+    console.log("App running on port 3000.");
 });
