6268ff307067ae2b533a35c55358ed53062f78f2
indiebreath <indiebreath@proton.me>
2024-09-03 21:05:44 +1000

    Final non-blog, non-docker version complete

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const router = express.Router();
 
 const port = 3000;
 
-app.use("/", express.static(__dirname + "/views"));
-app.use(express.static(__dirname + "/public"));
+app.use("/", express.static(__dirname + "/pages"));
+app.use("/public", express.static(__dirname + "/public"));
 
 router.get("/", function (req, res) {
-    res.sendFile(__dirname + "/views/index.html");
+    res.sendFile(__dirname + "/pages/index.html");
 });
+
+app.use("/", router);
 
 app.listen(port, () => {
     console.log("App running on port 3000.");
